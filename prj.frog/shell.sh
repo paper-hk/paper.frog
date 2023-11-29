0b5ede2c46dc4a01b3b2d61b4abfc0b4885796c9
paper <none.none>
2023-11-29 21:01:29 +0000

    Simplify documentation.

--- prj.frog/shell.sh
+++ prj.frog/shell.sh
@@ -1,31 +1,20 @@
 #!/usr/bin/env bash
 
-#------------------------------------------------------------------
 # Go to the directory containing this script.
-
 cd $(dirname $0)
 
-#------------------------------------------------------------------
 # Install the virtual environment
-
 python3 -m venv ./.venv
 
-#------------------------------------------------------------------
 # Activate the virtual environment.
-
 source ./.venv/bin/activate 
 
-#------------------------------------------------------------------
 # Have [pip] upgrade itself to the latest version.
-
 pip install --upgrade pip
 
-#------------------------------------------------------------------
 # Have it install all packages listed in [./requirements.txt].
-
 pip install -r ./requirements.txt
 
-#------------------------------------------------------------------
 # One of the packages is [pytest].  Run it to test our frog game.
 # But first, set [$PYTHONDONTWRITEBYTECODE] to avoid cluttering
 # the directory with an ugly __pycache__ folder.
@@ -33,7 +22,6 @@ pip install -r ./requirements.txt
 export PYTHONDONTWRITEBYTECODE=1
 pytest
 
-#------------------------------------------------------------------
 # Finally, launch a bash shell so we can play with our game.
 
 bash
